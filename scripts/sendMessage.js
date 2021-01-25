1b5716a3fc9d4a4d6edf6d8fb2de316c3088dc8f
Pak Vladimir <vladpak555@gmail.com>
2021-01-26 01:26:40 +0500

    Added built-in validation for form input. The validation was provided by Formspree code. Also started responsive design for the viewport (766px and less) - completed carousel in the header section.

--- scripts/sendMessage.js
+++ scripts/sendMessage.js
@@ -1,36 +1,47 @@
   window.addEventListener("DOMContentLoaded", function() {
-
     // get the form elements defined in your form HTML above
+    var inputName = document.getElementById("name");
+    var inputEmail = document.getElementById("email");
+    var inputMsg = document.getElementById("message");
 
     var form = document.getElementById("my-form");
-    // var button = document.getElementById("my-form-button");
     var status = document.getElementById("successNotice");
 
     // Success and Error functions for after the form is submitted
-
     function success() {
       form.reset();
       status.classList.add("success");
-      status.innerHTML = "Thanks!";
+      status.innerHTML = "Thanks! Your message was sent successfully!";
     }
 
     function error() {
-      status.classList.add("error");
-      status.innerHTML = "Oops! There was a problem.";
+        form.reset();
+        status.classList.add("error");
+        status.innerHTML = "Empty input. Please check.";
     }
 
-    // handle the form submission event
+    function invalidInput() {
+        form.reset();
+        status.classList.add("invalidInput");
+        status.innerHTML = "Invalid input. Please check.";
+    }
 
+
+    // handle the form submission event
     form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
+        if(inputName.value.length == 0 || inputEmail.value.length == 0 || inputMsg.value.length == 0){
+          invalidInput();
+        }
+        else{
+          ev.preventDefault();
+          var data = new FormData(form);
+          ajax(form.method, form.action, data, success, error, invalidInput);
+        }
     });
   });
 
   // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
+  function ajax(method, url, data, success, error, invalidInput) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
@@ -38,7 +49,8 @@
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
       if (xhr.status === 200) {
         success(xhr.response, xhr.responseType);
-      } else {
+      }
+      else {
         error(xhr.status, xhr.response, xhr.responseType);
       }
     };
