960041223481863a96750ddee2472fc5dcf83f66
Pak Vladimir <vladpak555@gmail.com>
2021-01-27 16:12:03 +0500

    Bug fixed: issue with open/close pop-up

--- scripts/openClosePopUp.js
+++ scripts/openClosePopUp.js
@@ -2,23 +2,26 @@ var btnForOpen = document.getElementById("popupDownload");
 
 document.getElementById("navBtnDowload").addEventListener("click", function() {
   btnForOpen.classList.add("openPopUp");
-  disableScrolling();
+  document.body.classList.add("openPopUpBlockBody");
+  //disableScrolling();
 });
 
 document.getElementById("navigationBtn").addEventListener("click", function() {
   btnForOpen.classList.add("openPopUp");
-  disableScrolling();
+  document.body.classList.add("openPopUpBlockBody");
+  //disableScrolling();
 });
 
 document.getElementById("closePopUp").addEventListener("click", function() {
   btnForOpen.classList.remove("openPopUp");
-  enableScrolling();
+  document.body.classList.remove("openPopUpBlockBody");
+  //enableScrolling();
 });
 
 
 function disableScrolling(){
-    var x=window.scrollX;
-    var y=window.scrollY;
+    var x=window.pageYOffset;
+    var y=window.pageXOffset;
     window.onscroll=function(){window.scrollTo(x, y);};
 }
 
